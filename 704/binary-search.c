d9a951bb736ecb59e5ad517333aadfb07e6e0141
shen <seeeyetoeye314@gmail.com>
2022-05-06 00:08:09 +0800

    Added 704

---
+++ 704/binary-search.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int search(int* nums, int numsSize, int target);
+
+int main() {
+    int arr[] = {2, 7, 11, 15, 23, 56 ,71};
+
+    int targetIdx = search(&arr, 7, 15);
+
+    printf("The target index is: %d\n", targetIdx);
+
+    return 0;
+}
+
+int search(int* nums, int numsSize, int target) {
+    int l=0, r = numsSize-1;
+
+    while (l<=r) {
+        int m = l+(r-l)/2;
+        if (nums[m] == target) return m;
+        else if (nums[m] < target) l = m+1;
+        else r = m-1;
+    }
+    return -1;
+}

02f1128a75cf105ade5504819dbb47b4c4a5dfad
MY Shum <43083625+ShingShen@users.noreply.github.com>
2022-05-17 11:40:40 +0800

    Create 1108.c

---
+++ 1108/1108.c
@@ -0,0 +1,20 @@
+// 1108. Defanging an IP Address
+#include <stdio.h>
+
+char* defangIPaddr(char* address);
+
+int main() 
+{
+    char* addr = "255.100.50.0";
+    printf("%s\n", defangIPaddr(addr));
+    return 0;
+}
+
+char* defangIPaddr(char* address)
+{
+    char* newAddr = (char*)malloc(30*sizeof(char));
+    int a = 0, b = 0, c = 0, d = 0;
+    sscanf(address, "%d.%d.%d.%d", &a, &b, &c, &d);
+    sprintf(newAddr, "%d[.]%d[.]%d[.]%d", a, b, c, d);
+    return newAddr;
+}

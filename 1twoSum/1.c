0107ea9d16f8c996a90469cfba3c3cb61e77213c
MY Shum <43083625+ShingShen@users.noreply.github.com>
2022-05-19 02:05:13 +0800

    Update 1.c

--- 1twoSum/1.c
+++ 1twoSum/1.c
@@ -18,7 +18,7 @@ typedef struct {
 } MyHashMap;
 
 int getIndex(int key) {
-    return (key % HASH_SIZE);
+    return (abs(key) % HASH_SIZE);
 }
 
 MyHashMap* myHashMapCreate() 
@@ -70,29 +70,6 @@ int myHashMapGet(MyHashMap* obj, int key)
     return -1;
 }
 
-// void myHashMapRemove(MyHashMap* obj, int key) 
-// {
-//     int index = getIndex(key);
-//     struct HashNode* curList = obj->hashTable[index];
-//     struct HashNode* preNode = NULL;
-    
-//     while(curList) {
-//         if (curList->key == key) {
-//             struct HashNode* delNode = curList;
-//             if (preNode) {
-//                 preNode->next = curList->next;
-//             } else {
-//                 obj->hashTable[index] = curList->next;
-//             }
-//             free(delNode);
-//             return;
-//         }
-//         preNode = curList;
-//         curList = curList->next;
-//     }
-//     return;
-// }
-
 void myHashMapFree(MyHashMap* obj) 
 {
     for (int i = 0; i < obj->size; i++) {
@@ -112,62 +89,16 @@ void myHashMapFree(MyHashMap* obj)
 int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     MyHashMap* hashMap = myHashMapCreate();
-    *returnSize = 2;
-    int* res = (int*)malloc(sizeof(int)*2);
+    int* res = calloc((*returnSize = 2), sizeof(int));
     for (int i = 0; i < numsSize; i++) {
         if (myHashMapGet(hashMap, target - nums[i]) != -1) {
             res[0] = myHashMapGet(hashMap, target - nums[i]);
             res[1] = i;
             myHashMapFree(hashMap);
-            *returnSize = 2;
             return res;
         }
         myHashMapPut(hashMap, nums[i], i);
     }
     myHashMapFree(hashMap);
-    *returnSize = 0;
-    return NULL;
+    return res;
 }
-
-// typedef struct {
-//     int val;
-//     int idx;
-//     UT_hash_handle hh;
-// } hash_table;
-
-// int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-//     int* res = calloc((*returnSize=2), sizeof(int));
-//     hash_table *numsHash=NULL, *elm=NULL, *tmp=NULL;
-    
-//     for(int i=0; i<numsSize; i++) {
-//         int another = target-nums[i];
-//         HASH_FIND_INT(numsHash, &another, elm);
-//         if (elm) {
-//             res[0] = elm->idx;
-//             res[1] = i;
-//             break;
-//         } else {
-//             elm = malloc(sizeof(hash_table));
-//             elm->val = nums[i]; 
-//             elm->idx = i;
-//             HASH_ADD_INT(numsHash, val, elm);
-//         }
-//     }
-//     HASH_ITER(hh, numsHash, elm, tmp) {
-//         HASH_DEL(numsHash, elm); 
-//         free(elm); 
-//     }
-//     return res;
-// }
-
-/**
- * Your MyHashMap struct will be instantiated and called as such:
- * MyHashMap* obj = myHashMapCreate();
- * myHashMapPut(obj, key, value);
- 
- * int param_2 = myHashMapGet(obj, key);
- 
- * myHashMapRemove(obj, key);
- 
- * myHashMapFree(obj);
-*/

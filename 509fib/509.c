3fd27fb74e9db7c3bb698053491272cc0c66f3e4
MY Shum <43083625+ShingShen@users.noreply.github.com>
2022-05-18 17:06:58 +0800

    Create 509.c

---
+++ 509fib/509.c
@@ -0,0 +1,34 @@
+// 509. Fibonacci Number
+#include <stdio.h>
+
+int recursionFib(int n);
+int dpFib(int n);
+
+int main() 
+{
+    int num = 9;
+    
+    printf("(Recursion) The Fibonacci number is: %d\n", recursionFib(num));
+    printf("(DP)        The Fibonacci number is: %d\n", dpFib(num));
+
+    return 0;
+}
+
+int recursionFib(int n)
+{
+    if (n <= 1) return;
+    return recursionFib(n-1) + recursionFib(n-2);
+}
+
+int dpFib(int n)
+{
+    int f[n+2], i;
+    f[0] = 0;
+    f[1] = 1;
+    
+    for (i = 2; i <= n; i++) {
+      f[i] = f[i-1] + f[i-2];
+    }
+    
+    return f[n];
+}

d03099684003da75c25177a936ad54e317f6db33
shen <seeeyetoeye314@gmail.com>
2022-10-15 00:24:32 +0800

    Added 146

--- 146lruCache/146.c
+++ 146lruCache/146.c
@@ -1,25 +1,25 @@
 // 146. LRU Cache
 
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdio.h>
 
+#define MAX_SIZE 10000
 
-#define HASHSIZE 10000
-
-struct node {
-    int value;
-    bool valid;
-    struct node* prev;
-    struct node* next;
-};
+typedef struct _Node {
+    int key;
+    int val;
+    struct _Node* prev;
+    struct _Node* next;
+} Node;
 
 typedef struct {
-    struct node* head;
-    struct node* tail;
-    struct node* myhash;
-    int capacity;
-    int size;
+    int maxCap;
+    int curCap;
+    Node* head;
+    Node* tail;
 } LRUCache;
+
+Node* map[MAX_SIZE];
 
 LRUCache* lRUCacheCreate(int capacity);
 int lRUCacheGet(LRUCache* obj, int key);
@@ -28,51 +28,128 @@ void lRUCacheFree(LRUCache* obj);
 
 int main()
 {
-    LRUCache* obj = lRUCacheCreate(2);
+    int lRUMaxCap = 2;
+    LRUCache* cache = lRUCacheCreate(lRUMaxCap);
+    lRUCachePut(cache, 1, 1);
+    puts("After Put [1, 1]:");
+    puts("Cache is:");
+    printf("[[%d, %d]]\n\n", cache->head->next->key, cache->head->next->val);
+    
+    lRUCachePut(cache, 2, 2);
+    puts("After Put [2, 2]:");
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
+    
+    printf("After Get key 1 = %d\n", lRUCacheGet(cache, 1));
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
+    
+    lRUCachePut(cache, 3, 3);
+    puts("After Put [3, 3]:");
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
+
+    printf("After Get key 2 = %d\n", lRUCacheGet(cache, 2));
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
+
+    lRUCachePut(cache, 4, 4);
+    puts("After Put [4, 4]:");
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
+
+    printf("After Get key 1 = %d\n", lRUCacheGet(cache, 1));
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
+
+    printf("After Get key 3 = %d\n", lRUCacheGet(cache, 3));
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
+    
+    printf("After Get key 4 = %d\n", lRUCacheGet(cache, 4));
+    puts("Cache is:");
+    printf("[[%d, %d], [%d, %d]]\n\n", cache->head->next->key, cache->head->next->val, cache->head->next->next->key, cache->head->next->next->val);
 }
 
 LRUCache* lRUCacheCreate(int capacity)
 {
-    LRUCache* obj = (LRUCache*)malloc(sizeof(LRUCache));
-    obj->myhash = (struct node*)calloc(HASHSIZE, sizeof(struct node));
-    obj->capacity = capacity;
-    obj->size = 0;
-    obj->head = NULL;
-    obj->tail = NULL;
-    return obj;
+    LRUCache* cache;
+    cache = malloc(sizeof(LRUCache));
+    cache->maxCap = capacity;
+    cache->curCap = 0;
+
+    cache->head = malloc(sizeof(Node));
+    cache->tail = malloc(sizeof(Node));
+    cache->head->next = cache->tail;
+    cache->tail->prev = cache->head;
+
+    for(int i = 0; i < MAX_SIZE; i++) map[i] = NULL;
+
+    return cache;
+}
+
+void rmNode(Node* currNode) 
+{
+    currNode->prev->next = currNode->next;
+    currNode->next->prev = currNode->prev;
+}
+
+void rmTail(LRUCache* obj)
+{
+    Node* tmp;
+    Node* tail = obj->tail;
+    tmp = tail->prev;
+    
+    map[tmp->key] = NULL;
+    rmNode(tmp);
+    free(tmp);
+}
+
+void addFront(LRUCache* obj, Node* currNode)
+{
+    Node* head = obj->head;
+    currNode->next = head->next;
+    currNode->prev = head;
+    head->next->prev = currNode;
+    head->next = currNode;
 }
 
 int lRUCacheGet(LRUCache* obj, int key)
 {
-    int value;
-    if(obj->myhash[key].valid) {
-
-    }
+    if(obj->curCap == 0) return -1;
+    if(map[key] == NULL) return -1;
+    int val = map[key]->val;
+    rmNode(map[key]);
+    addFront(obj, map[key]);
+    return val;
 }
 
 void lRUCachePut(LRUCache* obj, int key, int value)
 {
-
+    if(map[key] == NULL) {
+        if(obj->curCap == obj->maxCap) {
+            rmTail(obj);
+            obj->curCap--;
+        }
+        obj->curCap++;
+        Node* tmp = malloc(sizeof(Node));
+        tmp->key = key;
+        tmp->val = value;
+        map[key] = tmp;
+        addFront(obj, tmp);
+    } else {
+        rmNode(map[key]);
+        map[key]->val = value;
+        addFront(obj, map[key]);
+    }
 }
 
 void lRUCacheFree(LRUCache* obj)
 {
-    free(obj->myhash);
+    for(int i = 0; i < MAX_SIZE; i++) {
+        if(map[i]) free(map[i]);
+    }
+    free(obj->head);
+    free(obj->tail);
     free(obj);
-}
-
-void rmLru(LRUCache* obj)
-{
-    struct node* rmNode = obj->tail;
-    
-}
-
-void bringFront(LRUCache* obj, struct node* recentNode, int value)
-{
-
-}
-
-void add(LRUCache* obj, struct node* newNode, int value)
-{
-    
 }

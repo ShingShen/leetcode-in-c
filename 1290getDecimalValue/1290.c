b7fdf3eb14e7ac61bf6c7d84366885b266230b50
shen <seeeyetoeye314@gmail.com>
2022-10-15 12:26:23 +0800

    Add new solution in 1290

--- 1290getDecimalValue/1290.c
+++ 1290getDecimalValue/1290.c
@@ -7,25 +7,46 @@ struct ListNode {
     struct ListNode* next;
 } ListNode;
 
-int getDecimalValue(struct ListNode* head);
+int getDecimalValue1(struct ListNode* head);
+int getDecimalValue2(struct ListNode* head);
 struct ListNode* newNode(int newVal);
 
 int main()
 {
-    struct ListNode* list = newNode(1);
-    list->next = newNode(0);
-    list->next->next = newNode(1);
+    struct ListNode* list1 = newNode(1);
+    list1->next = newNode(0);
+    list1->next->next = newNode(1);
 
-    printf("The decimal value of the number: %d\n", getDecimalValue(list));
+    int decimalVal1 = getDecimalValue1(list1);
+    puts("Solution 1:");
+    printf("The decimal value of the number: %d\n", decimalVal1);
+
+    struct ListNode* list2 = newNode(1);
+    list2->next = newNode(0);
+    list2->next->next = newNode(1);
+
+    int decimalVal2 = getDecimalValue2(list2);
+    puts("Solution 2:");
+    printf("The decimal value of the number: %d\n", decimalVal2);
 
     return 0;
 }
 
-int getDecimalValue(struct ListNode* head)
+int getDecimalValue1(struct ListNode* head)
 {
     while (head->next) {
         head->val = (head->val)*2;
-        head->next->val = head->val + head->next->val;
+        head->next->val += head->val;
+        head = head->next;
+    }
+    return head->val;
+}
+
+int getDecimalValue2(struct ListNode* head)
+{
+    while (head->next) {
+        head->val = head->val<<1;
+        head->next->val += head->val;
         head = head->next;
     }
     return head->val;

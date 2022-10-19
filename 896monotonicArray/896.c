888d8e2440e412a1002ebfd5dd9bca0c36fa8722
shen <seeeyetoeye314@gmail.com>
2022-10-19 23:13:17 +0800

    896: Added Solution 3

--- 896monotonicArray/896.c
+++ 896monotonicArray/896.c
@@ -4,22 +4,29 @@
 
 bool isMonotonic1(int* nums, int numsSize);
 bool isMonotonic2(int* nums, int numsSize);
+bool isMonotonic3(int* nums, int numsSize);
 
 int main()
 {
-    int arr[] = {1, 2, 2, 3, 0};
+    int arr[] = {1, 2, 2, 3, 3};
     int arrSize = sizeof arr / sizeof *arr;
     printf("sizeof arr: %ld\n", sizeof arr);
     printf("sizeof *arr: %ld\n", sizeof *arr);
     printf("Array size is: %d\n", arrSize);
-    puts("Solution1:");
+    puts("Solution 1:");
     if (isMonotonic1(arr, arrSize)) {
         puts("Array is monotonic");
     } else {
         puts("Array is not monotonic");
     }
-    puts("Solution2:");
+    puts("Solution 2:");
     if (isMonotonic2(arr, arrSize)) {
+        puts("Array is monotonic");
+    } else {
+        puts("Array is not monotonic");
+    }
+    puts("Solution 3:");
+    if (isMonotonic3(arr, arrSize)) {
         puts("Array is monotonic");
     } else {
         puts("Array is not monotonic");
@@ -48,4 +55,14 @@ bool isMonotonic2(int* nums, int numsSize)
         if (inc == false && dec == false) return false;
     }
     return true;
+}
+
+bool isMonotonic3(int* nums, int numsSize)
+{
+    bool inc = true, dec = true;
+    for(int i = 0; i < numsSize-1; i++) {
+        inc &= (nums[i] <= nums[i+1]);
+        dec &= (nums[i] >= nums[i+1]);
+    }
+    return inc || dec;
 }
